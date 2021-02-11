ff937f2c4c97e33727cc039a209ec301e4d262d3
Pedro Paz <pedrogtpaz@gmail.com>
2021-02-11 15:07:35 -0300

    random button

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import requests
+from numpy.random import randint
 from variaveis import DATABASE
 
 app = Flask(__name__)
@@ -61,18 +62,42 @@ def atualizar_query():
 # sg.theme("SystemDefaultForReal")
 
 req = []
-layout = [
+
+layout_inicial = [
+    [sg.Button("Pesquisar")],
+    [sg.Button("Estudar")]
+]
+
+layout_estudar = [
+    [sg.Button("Random")],
+    [sg.Text(text="Pressione random para receber uma palavra aleatória", key='-KANJI-', auto_size_text=True)],
+    [sg.Button("Mostrar leitura"), sg.Button("Mostrar significado")],
+    [sg.Text(text="Pressione mostrar leitura para ver a leitura", key='-KANA-', auto_size_text=True)],
+    [sg.Text(text="Pressione mostrar significado para ver o significado", key='-ENGLISH-', auto_size_text=True)]
+]
+
+layout_pesquisar = [
     [sg.Input(key='-BUSCA-')],
-    [sg.Button("Pesquisar", bind_return_key=True, enable_events=True)],
+    [sg.Button("Buscar", bind_return_key=True, enable_events=True)],
     [sg.Table(values=iniciar_busca(), headings=cabecalho, key='-RESULT-')],
     [sg.Button("Adicionar")],
     [sg.Table(values=atualizar_query(), headings=cabecalho, key='-TABLE-')]
 ]
 
-window = sg.Window("titulo", layout)
+window = sg.Window("inicial", layout_inicial, size=(500, 500))
 busca = []
+leitura = None
+significado = None
 while True:
     event, values = window.read()
+
+    if event == 'Pesquisar':
+        window.close()
+        window = sg.Window('pesquisar', layout_pesquisar, size=(500, 500))
+
+    if event == 'Estudar':
+        window.close()
+        window = sg.Window('estudar', layout_estudar, size=(500, 500))
 
     kanji = ''
     kana = ''
@@ -84,10 +109,24 @@ while True:
     col_4 = []
     i = 1
 
+    if event == 'Random':
+        cards = Card.query.all()
+        x = randint(len(cards))
+        card = cards[x]
+        leitura = card.kana
+        significado = card.english
+        window['-KANJI-'].update(value=card.kanji)
+
+    if event == 'Mostrar leitura':
+        window['-KANA-'].update(value=leitura)
+
+    if event == 'Mostrar significado':
+        window['-ENGLISH-'].update(value=significado)
+
     if event == sg.WINDOW_CLOSED:
         break
 
-    if event == 'Pesquisar' and len(values['-BUSCA-']):
+    if event == 'Buscar' and len(values['-BUSCA-']):
         req = requests.get('https://jisho.org/api/v1/search/words?', params={'keyword':values['-BUSCA-']})
         json = req.json()
         busca = []
